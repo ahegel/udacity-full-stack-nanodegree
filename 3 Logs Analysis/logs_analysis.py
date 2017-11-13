a82cf50ab16f9053549dd423bb24a41bf67f1523
Allison Hegel <ahegel@gmail.com>
2017-11-13 10:10:26 -0800

    PEP8

--- 3 Logs Analysis/logs_analysis.py
+++ 3 Logs Analysis/logs_analysis.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 DBNAME = 'news'
 
+
 def executeQuery(query):
     '''
     Take a string query and execute the query.
@@ -16,6 +17,7 @@ def executeQuery(query):
     rows = c.fetchall()
     db.close()
     return rows
+
 
 def top_n_articles(n):
     '''
@@ -31,6 +33,7 @@ def top_n_articles(n):
     top_n_articles = executeQuery(query)
     return top_n_articles
 
+
 def popular_authors():
     '''
     Return the most popular authors by number of views.
@@ -45,6 +48,7 @@ def popular_authors():
     popular_authors = executeQuery(query)
     return popular_authors
 
+
 def days_with_error(n):
     '''
     Return days when more than n% of requests were errors.
@@ -52,7 +56,7 @@ def days_with_error(n):
     query = """SELECT day, perc FROM (
                  SELECT day, ROUND(
                    (SUM(requests)/(SELECT COUNT(*) FROM log
-                                   WHERE SUBSTRING(CAST(log.time AS text), 0, 11) = day) * 100), 2)
+               WHERE SUBSTRING(CAST(log.time AS text), 0, 11) = day) * 100), 2)
                  AS perc FROM (
                    SELECT SUBSTRING(CAST(log.time AS text), 0, 11) AS day,
                      COUNT(*) AS requests
@@ -75,13 +79,17 @@ if __name__ == '__main__':
     top_authors = popular_authors()
     error_days = days_with_error(error_perc)
     with open('output.txt', 'w') as f:
-        f.write('1. What are the most popular {} articles of all time?\n'.format(n_articles))
+        s1 = '1. What are the most popular {} articles of all time?\n'
+        f.write(s1.format(n_articles))
         for article in top_3_articles:
             f.write(article[0] + ', ' + str(article[1]) + ' views\n')
         f.write('\n2. Who are the most popular article authors of all time?\n')
         for author in top_authors:
             f.write(author[0] + ', ' + str(author[1]) + ' views\n')
-        f.write('\n3. On which days did more than {}% of requests lead to errors?\n'.format(error_perc))
+        s3 = ('\n3. On which days did more than '
+              '{}% of requests lead to errors?\n')
+        f.write(s3.format(error_perc))
         for error in error_days:
             clean_date = datetime.strptime(error[0], '%Y-%m-%d')
-            f.write(clean_date.strftime('%B %-d, %Y') + ', ' + str(error[1]) + '% errors')
+            f.write(clean_date.strftime('%B %-d, %Y') +
+                    ', ' + str(error[1]) + '% errors')
