71a76bff7679c74047ccd2b6abdb1914caf13b68
Allison Hegel <ahegel@gmail.com>
2017-11-12 17:29:38 -0800

    Add logs analysis project

---
+++ 3 Logs Analysis/README.md
@@ -0,0 +1,77 @@
+# Logs Analysis with SQL
+
+This project builds an internal reporting tool for a newspaper website that determines what kinds of articles the site's readers like. I use SQL to analyze a database with over a million rows and answer queries with the data.
+
+This project is part of the Udacity [Full Stack Web Developer Nanodegree](https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004).
+
+## Technologies Used
+
+* SQL
+* Python
+* [Vagrant](https://www.vagrantup.com/)
+* [VirtualBox](https://www.virtualbox.org/)
+
+## Setup
+
+1. Ensure that Python, the python package [psycopg2](https://pypi.python.org/pypi/psycopg2), [Vagrant](https://www.vagrantup.com/), and [VirtualBox](https://www.virtualbox.org/) are installed.
+2. Download or clone the [fullstack-nanodegree-vm](https://github.com/udacity/fullstack-nanodegree-vm) repository.
+3. Download the [SQL database](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip), unzip, and save `newsdata.sql` in the vagrant directory.
+4. Navigate to the vagrant folder in the terminal and enter `vagrant up` to bring the server online, followed by `vagrant ssh` to log in.
+5. To run the SQL queries directly, navigate to the vagrant directory with `cd /vagrant`, then enter `psql -d news -f newsdata.sql` to connect to and run the project database.
+6. To execute the program in this repo, run `python logs_analysis.py`.
+
+## Database
+
+The database contains three tables:
+
+* The `articles` table includes information about news articles and their contents.
+* The `authors` table includes information about the authors of articles.
+* The `log` table includes one entry for each time a user has accessed the news site.
+
+## Results
+
+You can also view the output of the SQL commands in the `output.txt` file in this repo.
+
+1. What are the most popular three articles of all time?
+
+    ```sql
+    SELECT articles.title, COUNT(*) as views
+    FROM articles INNER JOIN log
+    ON log.path LIKE CONCAT('%', articles.slug, '%')
+    WHERE log.status like '200%'
+    GROUP BY articles.title
+    ORDER BY views DESC
+    LIMIT 3
+    ```
+
+2. Who are the most popular article authors of all time?
+
+    ```sql
+    SELECT authors.name, COUNT(*) as views
+    FROM articles
+    INNER JOIN authors ON articles.author = authors.id
+    INNER JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%')
+    WHERE log.status like '200%'
+    GROUP BY authors.name
+    ORDER BY views DESC
+    ```
+
+3. On which days did more than 1% of requests lead to errors?
+
+    ```sql
+    SELECT day, perc FROM (
+        SELECT day, ROUND(
+            (SUM(requests)/(SELECT COUNT(*) FROM log
+                            WHERE SUBSTRING(CAST(log.time AS text), 0, 11) = day) * 100), 2)
+        AS perc FROM (
+            SELECT SUBSTRING(CAST(log.time AS text), 0, 11) AS day,
+                COUNT(*) AS requests
+            FROM log
+            WHERE status LIKE '%404%'
+            GROUP BY day)
+        AS log_percentage
+        GROUP BY day
+        ORDER BY perc DESC)
+    AS result
+    WHERE perc >= 1
+    ```
---
+++ 3 Logs Analysis/logs_analysis.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python2.7
+
+import psycopg2
+from datetime import datetime
+
+DBNAME = 'news'
+
+def executeQuery(query):
+    '''
+    Take a string query and execute the query.
+    Return a list of tuples.
+    '''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+    return rows
+
+def top_n_articles(n):
+    '''
+    Return the top n articles by number of views.
+    '''
+    query = """SELECT articles.title, COUNT(*) as views
+               FROM articles INNER JOIN log
+               ON log.path LIKE CONCAT('%', articles.slug, '%')
+               WHERE log.status like '200%'
+               GROUP BY articles.title
+               ORDER BY views DESC
+               LIMIT {}""".format(int(n))
+    top_n_articles = executeQuery(query)
+    return top_n_articles
+
+def popular_authors():
+    '''
+    Return the most popular authors by number of views.
+    '''
+    query = """SELECT authors.name, COUNT(*) as views
+               FROM articles
+               INNER JOIN authors ON articles.author = authors.id
+               INNER JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%')
+               WHERE log.status like '200%'
+               GROUP BY authors.name
+               ORDER BY views DESC"""
+    popular_authors = executeQuery(query)
+    return popular_authors
+
+def days_with_error(n):
+    '''
+    Return days when more than n% of requests were errors.
+    '''
+    query = """SELECT day, perc FROM (
+                 SELECT day, ROUND(
+                   (SUM(requests)/(SELECT COUNT(*) FROM log
+                                   WHERE SUBSTRING(CAST(log.time AS text), 0, 11) = day) * 100), 2)
+                 AS perc FROM (
+                   SELECT SUBSTRING(CAST(log.time AS text), 0, 11) AS day,
+                     COUNT(*) AS requests
+                   FROM log
+                   WHERE status LIKE '%404%'
+                   GROUP BY day)
+                 AS log_percentage
+                 GROUP BY day
+                 ORDER BY perc DESC)
+               AS result
+               WHERE perc >= {}""".format(int(n))
+    errors = executeQuery(query)
+    return errors
+
+if __name__ == '__main__':
+    n_articles = 3
+    error_perc = 1
+
+    top_3_articles = top_n_articles(n_articles)
+    top_authors = popular_authors()
+    error_days = days_with_error(error_perc)
+    with open('output.txt', 'w') as f:
+        f.write('1. What are the most popular {} articles of all time?\n'.format(n_articles))
+        for article in top_3_articles:
+            f.write(article[0] + ', ' + str(article[1]) + ' views\n')
+        f.write('\n2. Who are the most popular article authors of all time?\n')
+        for author in top_authors:
+            f.write(author[0] + ', ' + str(author[1]) + ' views\n')
+        f.write('\n3. On which days did more than {}% of requests lead to errors?\n'.format(error_perc))
+        for error in error_days:
+            clean_date = datetime.strptime(error[0], '%Y-%m-%d')
+            f.write(clean_date.strftime('%B %-d, %Y') + ', ' + str(error[1]) + '% errors')
